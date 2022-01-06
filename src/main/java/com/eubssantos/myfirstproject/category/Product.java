410a0fa2be7db0637b3d17d46dfa7ab1c0d4f8b9
Bruna <eubssantos@outlook.com>
2022-01-06 09:03:21 -0300

    Product memory

--- src/main/java/com/eubssantos/myfirstproject/category/Product.java
+++ src/main/java/com/eubssantos/myfirstproject/category/Product.java
@@ -1,24 +1,25 @@
 package com.eubssantos.myfirstproject.category;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-public class Category implements Serializable {
+public class Product implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
     private String name;
+    private Double price;
 
-    @JsonIgnore
-    private List<Product> products = new ArrayList<>();
+    private Category category;
 
-    public Category(Long id, String name) {
+    public Product() {
+    }
+
+    public Product(Long id, String name, Double price, Category category) {
         super();
         this.id = id;
         this.name = name;
+        this.price = price;
+        this.category = category;
     }
 
     public Long getId() {
@@ -37,16 +38,28 @@ public class Category implements Serializable {
         this.name = name;
     }
 
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category){
+        this.category = category;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + (((id == null)) ? 0 : id.hashCode());
         return result;
-    }
-
-    public List<Product> getProducts() {
-        return products;
     }
 
     @Override
@@ -57,13 +70,12 @@ public class Category implements Serializable {
             return false;
         if(getClass() != obj.getClass())
             return false;
-        Category other = (Category) obj;
+        Product other = (Product) obj;
         if (id == null) {
             if (other.id != null)
                 return false;
         }  else if (!id.equals(other.id))
             return false;
         return true;
-        }
-
     }
+}
